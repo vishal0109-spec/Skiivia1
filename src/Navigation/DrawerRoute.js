6eac8c90c0a4c613ed895894f90ea3a1acba9ae0
Vishal <vishal01092@gmail.com>
2024-03-13 10:54:16 +0530

    Firestore working

--- src/Navigation/DrawerRoute.js
+++ src/Navigation/DrawerRoute.js
@@ -1,77 +1,96 @@
-import { View, Text } from 'react-native';
-import React, { useState } from 'react';
-import { DrawerContent, createDrawerNavigator } from '@react-navigation/drawer';
-import { Home } from '../Screens';
+import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {DrawerContent, createDrawerNavigator} from '@react-navigation/drawer';
 import Upload from '../Screens/Upload';
 import Account from '../Screens/Account';
 import TabRoutes from './TabRoutes';
-import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Home} from '../Screens';
+import Button from '../Components/CustomButton';
+import {account, home, upload} from '../Utils/img';
+import {tabBarstyles} from './tabBarStyle';
 
 const Drawer = createDrawerNavigator();
 
 function CustomDrawerContent() {
-    const [selectMenu, setSelectMenu] = useState(1);
-    const navigation = useNavigation();
-    const menuPress = (num) => {
-      setSelectMenu(num);
-      if (num == 1) {
-        navigation.navigate(Route.DrawerTab, {screen: Route.Reels});
-      }
-    };
-    return (
-      <View>
-        {/* <Button
-          style={[
-            style.drawerIconOpacity,
-            {backgroundColor: selectMenu == 1 ? '#75140C' : '#FFFFFF'},
-          ]}
-          btnStyle={[
-            style.drawerIconTxt,
-            {color: selectMenu == 1 ? '#FFFFFF' : '#000000'},
-          ]}
-          iconStyle={style.drawerIcon}
-          icon={video}
-          title="Reels"
-          onPress={() => {
-            menuPress(1);
-          }}
-        /> */}
-        {/* <Button
-          style={[
-            style.drawerIconOpacity,
-            {
-              backgroundColor: selectMenu == 2 ? '#75140C' : '#FFFFFF',
-            },
-          ]}
-          btnStyle={[
-            style.drawerIconTxt,
-            {color: selectMenu == 2 ? '#FFFFFF' : '#000000'},
-          ]}
-          iconStyle={style.drawerIcon}
-          icon={chat}
-          title="chat"
-          onPress={() => {
-            menuPress(2);
-          }}
-        /> */}
-        <TouchableOpacity style={{backgroundColor:'red'}}>
-            <Text>Home</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  const [selectMenu, setSelectMenu] = useState(1);
+  const navigation = useNavigation();
+  const menuPress = num => {
+    setSelectMenu(num);
+    if (num == 1) {
+      navigation.navigate('Home');
+    }
+    if (num == 2) {
+      navigation.navigate('Upload');
+    }
+    if (num == 3) {
+      navigation.navigate('Account');
+    }
+  };
+  return (
+    <View>
+      <Button
+        style={[
+          tabBarstyles.drawerIconOpacity,
+          {backgroundColor: selectMenu == 1 ? '#75140C' : '#FFFFFF'},
+        ]}
+        title="Home"
+        icon={home}
+        iconStyle={tabBarstyles.drawerIcon}
+        btnStyle={[
+          tabBarstyles.drawerIconTxt,
+          {color: selectMenu == 1 ? '#FFFFFF' : '#000000'},
+        ]}
+        onPress={() => {
+          menuPress(1);
+        }}
+      />
+      <Button
+        style={[
+          tabBarstyles.drawerIconOpacity,
+          {backgroundColor: selectMenu == 2 ? '#75140C' : '#FFFFFF'},
+        ]}
+        title="Upload"
+        icon={upload}
+        iconStyle={tabBarstyles.drawerIcon}
+        btnStyle={[
+          tabBarstyles.drawerIconTxt,
+          {color: selectMenu == 2 ? '#FFFFFF' : '#000000'},
+        ]}
+        onPress={() => {
+          menuPress(2);
+        }}
+      />
+      <Button
+        style={[
+          tabBarstyles.drawerIconOpacity,
+          {backgroundColor: selectMenu == 3 ? '#75140C' : '#FFFFFF'},
+        ]}
+        title="Account"
+        icon={account}
+        iconStyle={tabBarstyles.drawerIcon}
+        btnStyle={[
+          tabBarstyles.drawerIconTxt,
+          {color: selectMenu == 3 ? '#FFFFFF' : '#000000'},
+        ]}
+        onPress={() => {
+          menuPress(3);
+        }}
+      />
+    </View>
+  );
+}
 
 const DrawerRoute = () => {
   return (
-    <Drawer.Navigator screenOptions={{
+    <Drawer.Navigator
+      screenOptions={{
         headerShown: false,
-      }} drawerContent={props => <CustomDrawerContent />}>
-        <Drawer.Screen name = "TabRoutes" component={TabRoutes} />
-        <Drawer.Screen name = "Home" component={Home} />
-        <Drawer.Screen name = "Upload" component={Upload} />
-        <Drawer.Screen name = "Account" component={Account} />
+      }}
+      drawerContent={props => <CustomDrawerContent />}>
+      <Drawer.Screen name="TabRoutes" component={TabRoutes} />
     </Drawer.Navigator>
   );
 };
